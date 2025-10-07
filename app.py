41111118b83257f62aa47c81234c69e7e995fe33
Daniel Ivan <24308060610008@cetis61.edu.mx>
2025-10-06 21:00:30 -0600

    2

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html', a="Daniel Ivan Leyva Holguin")
+    return render_template('index.html')
 
-@app.route("/index1")
-def index1():
-    return render_template('index1.html')
+#@app.route("/index1")
+#def index1():
+#    return render_template('index1.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
